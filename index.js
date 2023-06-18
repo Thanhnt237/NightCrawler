0caa576b737f06b7520a6d9a8376532f4a1b6f57
ThanhNt <thanhnt@bytesoft.net>
2023-06-18 10:19:10 +0700

    feat: order

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const puppeteer=require('puppeteer');
-const xl = require('excel4node');
-const wb = new xl.Workbook();
-const fs = require('fs');
-const axios = require('axios');
+const xlsx = require('xlsx')
 
 /*
 =========================================================
@@ -11,28 +8,21 @@ const axios = require('axios');
 let username="thuocthanthien";
 let password="0985447007";
 let fileName = 'Excel.xlsx'
-let ENDPAGE = 0;
+let resultFileName = 'result.xlsx'
 /*
 =========================================================
 =========================================================
 */
 let crawlURL = 'https://giathuochapu.com/san-pham/page/';
-let redirectedURL = 'https://giathuochapu.com/dat-hang-nhanh/?swcfpc=1'
-
-let globalProducts = [];
-
-const ws = wb.addWorksheet('Sheet 1');
-const style = wb.createStyle({
-  font: {
-    color: '#000000',
-    size: 12,
-  },
-  numberFormat: '$#,##0.00; ($#,##0.00); -',
-});
+// let redirectedURL = 'https://giathuochapu.com/dat-hang-nhanh/?swcfpc=1'
 
 (async () => {
+  console.log("Remember all production must be handle on " + fileName)
   console.log("Starting program...")
-  const browser = await puppeteer.launch({headless: true});
+  const products = await getProd()
+  console.log("Going to order " + products.length + " products!")
+
+  const browser = await puppeteer.launch({headless: false});
 
   const page = await browser.newPage();
   await page.goto('https://giathuochapu.com/dang-nhap/');
@@ -42,121 +32,70 @@ const style = wb.createStyle({
   await loggin(page)
 
   console.log("Redirecting to product page...")
-  console.log("Get EndPage...")
-  await page.goto("https://giathuochapu.com/san-pham/?swcfpc=1");
-  await page.waitForSelector(".page-numbers").then(async ()=>{
-    ENDPAGE = await page.evaluate(()=>{
-      return Number(document.querySelectorAll(".page-numbers")[3].innerText)
-    })
-  })
-  console.log("Successful! We have " + ENDPAGE + " pages here!")
+  console.log("Start order...")
 
-  console.log("Crawling data...")
-  for(let i=1; i<= ENDPAGE; i++){
-    await page.goto(`${crawlURL}${i}/?swcfpc=1`);
-    console.log("Crawling page " + i + "/" + ENDPAGE)
-    await page.waitForSelector(".product-item", {timeout: 60000}).then(async ()=>{
-        const products = await page.evaluate(() => {
-          let items = document.querySelectorAll(".product-item");
-          let product = [];
-
-          items.forEach(async item => {
-            const imageSrc = item.querySelector(".product-thumb").querySelector(".post-thumbnail").children[0].attributes["src"].value
-            const title = item.querySelector(".entry-title") ? item.querySelector(".entry-title").innerText : ""
-
-            product.push({
-            title,
-            type: item.querySelector(".product-type") ? item.querySelector(".product-type").innerText.replace("Nhóm: ","") : "",
-            price: item.querySelector(".price") ? item.querySelector(".price").innerText.slice(0, -2) : "",
-            tag: item.querySelector(".product-tag") ? item.querySelector(".product-tag").innerText : "",
-            date: item.querySelector(".product-expire-date") ?  item.querySelector(".product-expire-date").innerText : "",
-              imageSrc
-          });
-
-          });
-          return product;
-        });
-
-        if(globalProducts.length === 0){
-          globalProducts = products
-        }else{
-          globalProducts = globalProducts.concat(products)
-        }
-      })
+  for (const product of products) {
+    await order(page, product.production, product.quantity)
   }
 
-  console.log(globalProducts.length + " items was crawled!")
-  console.log("Creating Excel File...")
-
-  ws.cell(1, 1)
-    .string("Nhãn")
-    .style(style);
-  ws.cell(1, 2)
-    .string("Nhóm")
-    .style(style);
-  ws.cell(1, 3)
-    .string("Giá tiền")
-    .style(style);
-  ws.cell(1, 4)
-      .string("Product Tag")
-      .style(style);
-  ws.cell(1, 5)
-      .string("Date")
-      .style(style);
-  ws.cell(1, 6)
-      .string("image")
-      .style(style);
-
-  globalProducts.forEach((item, i) => {
-    ws.cell(i+2, 1)
-      .string(item.title)
-      .style(style);
-    ws.cell(i+2, 2)
-      .string(item.type)
-      .style(style);
-    ws.cell(i+2, 3)
-      .string(item.price)
-      .style(style);
-    ws.cell(i+2, 4)
-        .string(item.tag)
-        .style(style);
-    ws.cell(i+2, 5)
-        .string(item.date)
-        .style(style);
-    ws.cell(i+2, 6)
-        .string(item.imageSrc)
-        .style(style);
-  });
-
-  wb.write(fileName);
-  console.log("Done! File named: " + fileName)
-
-  await crawlImage(browser)
+  await handleResult()
 
   console.log("Shutting down...")
   await browser.close();
-
 })();
 
-async function crawlImage(browser){
-  console.log("Preparing to crawl image...")
+async function getProd() {
+  let data = []
+  const buffer = xlsx.readFile(fileName)
 
-  console.log("Starting...")
-  for (const product of globalProducts) {
-    console.log(`crawling image ${product.title}.png`)
-    try{
-      const res = await axios.get(product.imageSrc, { responseType: 'stream' })
-      // const res = await newPage.goto(product.imageSrc, {timeout: 0, waitUntil: 'networkidle0'})
-      // const imageBuffer = Buffer.from(res.data, 'binary').toString('base64')
-      await fs.promises.writeFile(`./assets/${product.title}.jpg`, res.data)
-      // await new Promise(c => setTimeout(c, 1000));
-      console.log(`Success crawl image ${product.title}.png`)
-    }catch(error){
-      console.log(`Fail crawl image ${product.title}.png`)
-      console.log(error)
-    }
+  buffer.SheetNames.map(sheetName => {
+    data = data.concat(xlsx.utils.sheet_to_json(buffer.Sheets[sheetName]))
+  });
+
+  return data.map(c => ({
+    production: c.production,
+    quantity: c.quantity || 0
+  }))
+}
+const successProd = []
+const failProd = []
+
+async function order(page, production, quantity) {
+  try{
+    await page.goto("https://giathuochapu.com/san-pham/?swcfpc=1");
+    await page.waitForNavigation({ waitUntil: "domcontentloaded" })
+    console.log('Starting order ' + production)
+    await page.type('#quick-search', production);
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation()
+    console.log('Make order ' + quantity + ' products!')
+    await page.type('.quantity_products', quantity.toString())
+    successProd.push(production)
+    console.log('Order successful')
+  }catch(error){
+    failProd.push(production)
+    console.log('Order fail ' + production)
   }
-  console.log("All image scanned!")
+}
+
+async function handleResult() {
+  console.log('Handle order result...')
+  const result = {
+    successProd: successProd.map(c => ({production: c})),
+    failProd: failProd.map(c => ({production: c}))
+  }
+  
+  const workbook = xlsx.utils.book_new();
+  const workSheetSuccess = xlsx.utils.json_to_sheet(result.successProd)
+  const workSheetFail = xlsx.utils.json_to_sheet(result.failProd)
+  await xlsx.utils.book_append_sheet(workbook,workSheetSuccess,'Thanh cong')
+  await xlsx.utils.book_append_sheet(workbook,workSheetFail,'That bai')
+  console.log('Success! Result save on ' + resultFileName)
+  await xlsx.writeFileXLSX(workbook, resultFileName)
+}
+
+async function setTimeout(timeout) {
+  return await new Promise(c => setTimeout(c, timeout));
 }
 
 async function loggin(page) {
@@ -165,9 +104,7 @@ async function loggin(page) {
   await	page.type('#user_pass',password);
   await page.click("label[id='remember_description']")
   await	page.click("button[value='1']");
-  await	page.waitForNavigation({
-    timeout: '100000'
-  });
+  await	page.waitForNavigation();
 
   let current_url = page.url()
   if(current_url === 'https://giathuochapu.com/dang-nhap/'){
